6d9e42eb4f1ad32e94e5e80456f9416feb488788
Shahriyor Namozov <baxtiyarovich0102@gmail.com>
2025-08-06 17:47:08 +0500

    finished

--- server/utils/bookGenerator.js
+++ server/utils/bookGenerator.js
@@ -7,15 +7,17 @@ const LOCALES = {
   ja_JP: ja,
 }
 
+const GENRES = ["Fiction", "Science", "Romance", "Thriller", "Fantasy", "History"]
+
 export function generateBooks({ seed, page, region, likes, reviews }) {
   const localeData = LOCALES[region] || en
   const books = []
 
-  const baseSeed = seed + "-" + page
+  const baseSeed = `${seed}-${page}`
   const rng = seedrandom(baseSeed)
 
   for (let i = 0; i < 20; i++) {
-    const index = (page - 1) * 10 + i + 1
+    const index = (page - 1) * 20 + i + 1
     const bookSeed = `${baseSeed}-${i}`
     const bookRng = seedrandom(bookSeed)
 
@@ -24,18 +26,45 @@ export function generateBooks({ seed, page, region, likes, reviews }) {
       random: bookRng,
     })
 
+    // ISBN 10 belgidan iborat katta harf va raqamlar
     const isbn = faker.string.alphanumeric(10).toUpperCase()
+
+    // Title - 3 so‘zdan iborat so‘zlar (locale ga mos)
     const title = faker.lorem.words(3)
-    const authors = [faker.person.fullName(), faker.person.fullName()]
+
+    // Mualliflar - 2 ta to‘liq ism (locale ga mos)
+    const authors = [faker.name.fullName(), faker.name.fullName()]
+
+    // Nashriyot
     const publisher = faker.company.name()
 
+    // Mamlakat (locale ga mos)
+    const country = faker.location.country()
+
+    // Janr
+    const genre = faker.helpers.arrayElement(GENRES)
+
+    // Language qisqartmasi, masalan "en" "de"
+    const language = region.split("_")[0]
+
+    // Likes va reviews sonini fractional probabilistik hisoblash
     const bookLikes = generateFractionalAmount(likes, rng)
     const reviewCount = generateFractionalAmount(reviews, rng)
 
-    const reviewsArr = Array.from({ length: reviewCount }, () => ({
-      author: faker.person.fullName(),
-      text: faker.lorem.sentences(2),
-    }))
+    // Sharhlar ro‘yxati, har biri muallif va text bilan
+    const reviewsArr = Array.from({ length: reviewCount }, (_, idx) => {
+      const reviewSeed = `${bookSeed}-review-${idx}`
+      const reviewRng = seedrandom(reviewSeed)
+      const reviewFaker = new Faker({
+        locale: [localeData],
+        random: reviewRng,
+      })
+
+      return {
+        author: reviewFaker.name.fullName(),
+        text: reviewFaker.lorem.sentences(2),
+      }
+    })
 
     books.push({
       index,
@@ -43,6 +72,9 @@ export function generateBooks({ seed, page, region, likes, reviews }) {
       title,
       authors,
       publisher,
+      country,
+      genre,
+      language,
       likes: bookLikes,
       reviews: reviewsArr,
     })
